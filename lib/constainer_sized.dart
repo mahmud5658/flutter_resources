22a7df8d092a5089658568136e9fc09d5307d554
mahmud5658 <almahmuda5@gmail.com>
2024-02-29 13:14:52 +0600

    contianer widget

---
+++ lib/constainer_sized.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class ContainerSized extends StatelessWidget {
+  const ContainerSized({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Constainer and Sizedbox'),
+      ),
+    );
+  }
+}
