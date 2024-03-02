a0abb34cb27850f43da3174872c6bfe8cca3cae3
mahmud5658 <almahmuda5@gmail.com>
2024-03-02 11:36:20 +0600

    flutter resources

---
+++ lib/widgets/elevated_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ElevatedButtonw extends StatelessWidget {
+  const ElevatedButtonw({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Elevated Button Widget'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              height: 50,
+              width: 200,
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Theme.of(context).primaryColor),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    print('Elevated Button Click');
+                  },
+                  child: const Text(
+                    'Hello',
+                    style: TextStyle(
+                        fontSize: 20, color: Colors.white),
+                  )),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/text_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class TextButtonW extends StatelessWidget {
+  const TextButtonW({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Text Button Widget"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: TextButton(
+              onPressed: () {
+                print("Hello Button Click");
+              },
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all(const EdgeInsets.all(10)),
+                backgroundColor: MaterialStateProperty.all(Colors.yellow),
+                overlayColor: MaterialStateProperty.all(Colors.pink),
+                elevation: MaterialStateProperty.all(10),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(2),
+                  ),
+                ),
+              ),
+              child: const Text(
+                'Hello',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
