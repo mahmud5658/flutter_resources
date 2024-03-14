2ec8f6b4db81122a5933ca9b8fb1ba7730886437
mahmud5658 <almahmuda5@gmail.com>
2024-03-14 11:45:05 +0600

    Tab Bar Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 // import 'package:flutter_resources/widgets/animated_text.dart';
 // import 'package:flutter_resources/widgets/bottom_bar.dart';
 // import 'package:flutter_resources/widgets/form.dart';
-import 'package:flutter_resources/widgets/stack.dart';
+// import 'package:flutter_resources/widgets/stack.dart';
+import 'package:flutter_resources/widgets/tabbar.dart';
 // import 'package:flutter_resources/widgets/alert_dialog.dart';
 // import 'package:flutter_resources/widgets/bottom_sheet.dart';
 // import 'package:flutter_resources/widgets/dismissable.dart';
@@ -39,7 +40,7 @@ class MyApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(backgroundColor: Colors.purple),
         brightness: Brightness.dark,
       ),
-      home:  const StackWidget(),
+      home:  const TabBarWidget(),
     );
   }
 }
