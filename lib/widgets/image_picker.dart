e28f170b87c3f2dcd398ea917d32f6a5472e7eea
mahmud5658 <almahmuda5@gmail.com>
2024-03-14 12:13:28 +0600

    image picker

---
+++ lib/widgets/image_picker.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerWidget extends StatefulWidget {
+  const ImagePickerWidget({super.key});
+
+  @override
+  State<ImagePickerWidget> createState() => _ImagePickerWidgetState();
+}
+
+class _ImagePickerWidgetState extends State<ImagePickerWidget> {
+  ImagePicker _picker = ImagePicker();
+  XFile? file;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Picker'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.grey.shade400,
+              child: const Center(child: Text('Image not found')),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Pick Image'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
