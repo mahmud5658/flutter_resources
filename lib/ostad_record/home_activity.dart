9af46903b2abc9ff508f4036d6d4bc2fe78b22d0
mahmud5658 <almahmuda5@gmail.com>
2024-03-21 21:03:27 +0600

    flutter resources

--- lib/ostad_record/home_activity.dart
+++ lib/ostad_record/home_activity.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
-class HomeActivity extends StatelessWidget {
+class HomeActivity extends StatefulWidget {
   const HomeActivity({super.key});
+
+  @override
+  State<HomeActivity> createState() => _HomeActivityState();
+}
+
+class _HomeActivityState extends State<HomeActivity> {
+  int selectedIndex = 0;
+  
 
   mySnackBar(context, message) {
     return ScaffoldMessenger.of(context)
         .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void onTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
   }
 
   @override
@@ -16,12 +30,13 @@ class HomeActivity extends StatelessWidget {
           'Inventory',
           style: TextStyle(color: Colors.white),
         ),
-        titleSpacing: 40,
+        titleSpacing: 0,
         // centerTitle: true,
         toolbarHeight: 70,
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.green,
         // toolbarOpacity: .3,
         elevation: 6,
+        // leading: const Icon(Icons.menu,color: Colors.white,),
         actions: [
           IconButton(
               onPressed: () {
@@ -33,7 +48,7 @@ class HomeActivity extends StatelessWidget {
               )),
           IconButton(
             onPressed: () {
-                mySnackBar(context, "This is setting icon");
+              mySnackBar(context, "This is setting icon");
             },
             icon: const Icon(
               Icons.settings,
@@ -42,7 +57,7 @@ class HomeActivity extends StatelessWidget {
           ),
           IconButton(
             onPressed: () {
-                mySnackBar(context, "This is notification icon");
+              mySnackBar(context, "This is notification icon");
             },
             icon: const Icon(
               Icons.notifications,
@@ -50,6 +65,82 @@ class HomeActivity extends StatelessWidget {
             ),
           )
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          mySnackBar(context, "This floating action button");
+        },
+        elevation: 10,
+        backgroundColor: Colors.green,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Message"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+        currentIndex: selectedIndex,
+        onTap: (int index) {
+          onTapped(index);
+          if (index == 0) {
+            mySnackBar(context, "This Home menu");
+          }
+          if (index == 1) {
+            mySnackBar(context, "This is message menu");
+          }
+          if (index == 2) {
+            mySnackBar(context, "This is profile menu");
+          }
+        },
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+           const  DrawerHeader(
+              padding: EdgeInsets.all(0),
+              child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Colors.black),
+                accountName: Text('Abdullah Al Mahmud'),
+                accountEmail: Text('mahmud15-5658@diu.edu.bd'),
+                currentAccountPicture: Image(image: NetworkImage('https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=1380&t=st=1710860819~exp=1710861419~hmac=e309a2ccc2e8191406c07118b95d377dda81c782d2de5d66ecad3b55fa684a57'),
+                
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () => {
+                mySnackBar(context, "This is home title"),
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.phone),
+              title: const Text('Contact'),
+              onTap: () => {
+                mySnackBar(context, "This is contact title"),
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profile'),
+              onTap: () => {
+                mySnackBar(context, "This is profile title"),
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.email),
+              title: const Text('Email'),
+              onTap: () => {
+                mySnackBar(context, "This is email title"),
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
